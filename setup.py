40a1607e271bf120e8ddb10b27e005bd6806b5e2
Jeetendra Patil <jeet4320@users.noreply.github.com>
2019-07-17 16:11:56 -0500

    Update requests requirement as its old  (#3)
    * update requests
    
    * update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-CURRENT_VERSION = '0.12'
+CURRENT_VERSION = '0.13'
 
 setup(
   name = 'basegateway',
@@ -13,7 +13,7 @@ setup(
   download_url = 'https://github.com/bodyshopbidsdotcom/basegateway/tarball/{0}'.format(CURRENT_VERSION),
   keywords = ['api', 'gateway', 'http', 'REST'],
   install_requires = [
-    'requests==2.9.1'
+    'requests>=2.9.1'
   ],
   classifiers = [
     "Topic :: Internet :: WWW/HTTP",
